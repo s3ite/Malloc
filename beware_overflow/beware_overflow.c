fd9e6bb0fad57c2636db4c498dc0f33ba9786202
Guy Noe <Guynoedjoufaing@gmail.com>
2022-10-25 09:07:02 +0200

    beware_overflow.c

--- beware_overflow/beware_overflow.c
+++ beware_overflow/beware_overflow.c
@@ -2,6 +2,5 @@
 
 void *beware_overflow(void *ptr, size_t nmemb, size_t size)
 {
-    int hasoverflow = __builtin_umull_overflow(nmemb, size, ptr);
-    return hasoverflow == 0 ? ptr : NULL;
+    return __builtin_umull_overflow(nmemb, size, ptr) ? NULL : ptr;
 }
